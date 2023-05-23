d757ab30d43f2f0750f6c3950c3e5c5f87e1e770
Jorge Prendes <jorge.prendes@gmail.com>
2023-05-23 15:39:14 +0100

    Add hello-wasm

--- README.md
+++ README.md
@@ -9,7 +9,13 @@ In bash run:
 ```bash
 source <(curl -sSfL https://raw.githubusercontent.com/jprendes/docker-wasm/main/docker-wasm.sh)
 
-# Run a hello world wasm container
+# Run a hello-wasm container
+docker-wasm run --rm \
+    --platform=wasm32/wasi \
+    --runtime=io.containerd.wasmtime.v1 \
+    jorgeprendes420/hello-wasm
+
+# Run a rust compiled wasm container
 docker-wasm run --rm \
     --platform=wasm32/wasi \
     --runtime=io.containerd.wasmtime.v1 \
--- hello-wasm/build.sh
+++ hello-wasm/build.sh
@@ -3,11 +3,10 @@
 source ../docker-wasm.sh
 
 # Build the python wasm container image
-docker-wasm buildx build --provenance=false --platform=wasi/wasm32 -t python-wasm .
+docker-wasm buildx build --provenance=false --platform=wasi/wasm32 -t hello-wasm .
 
 # Run the container, running a tiny python script
 docker-wasm run --rm \
         --platform=wasi/wasm32 \
         --runtime=io.containerd.wasmtime.v1 \
-        python-wasm \
-        -c 'import os; print(os.uname())'
+        hello-wasm
